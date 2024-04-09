78a321f1856551f25158d8f486719b257dc7bfa8
Sumbul Naz <164656538+sumbulNaz8@users.noreply.github.com>
2024-04-09 15:10:54 +0500

    Add files via upload

---
+++ index.js
@@ -0,0 +1,35 @@
+#!/usr/bin/env node
+import inquirer from "inquirer";
+let todos = [];
+let condition = true;
+while (condition) {
+    let addTask = await inquirer.prompt([
+        {
+            name: 'todoItem',
+            type: 'input',
+            message: "What  you want to add in your Todolist?"
+        },
+        {
+            name: 'addmore',
+            type: 'confirm',
+            message: 'Do you want to add more?',
+            default: false
+        },
+        {
+            name: 'todoGrocery',
+            type: 'input',
+            message: "What you want to buy vegetable?",
+        },
+        {
+            name: 'addmore_Grocery',
+            type: 'confirm',
+            message: 'Do you want to add more todolist?',
+            default: false
+        }
+    ]);
+    todos.push(addTask.todoItem);
+    todos.push(addTask.todoGrocery);
+    condition = addTask.addmore;
+    condition = addTask.addmore_Grocery;
+    console.log(todos);
+}
